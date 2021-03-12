7a1babacf9225b257a020f041e6c8e56912f29cc
KovacAca <kovacaleksandar1@gmail.com>
2021-03-12 02:07:03 +0100

    hivHeader

--- how-it-works.php
+++ how-it-works.php
@@ -1,5 +1,24 @@
 <?php include "partials/header.php"; ?>
-
-  <h1 style='font-size: 9rem; text-align: center;'>How it works</h1>
+<!-- hivHeader -->
+<section class="hivHeader">
+  <h2 class="container">How it works</h2>
+  <article class="container">
+    <div>
+      <i class="fas fa-user-circle"></i>
+      <h3>Create an account</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta eos tempora itaque architecto iure nisi nesciunt eum quos culpa fugiat?</p>
+    </div>
+    <div>
+      <i class="fas fa-drum"></i>
+      <h3>Create an account</h3>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas numquam cumque dolore hic iste inventore voluptatibus nulla laudantium ab officia!</p>
+    </div>
+    <div>
+      <i class="fas fa-cloud-download-alt"></i>
+      <h3>Create an account</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum corrupti porro natus vitae dolores tempore dolore? Minima deleniti est ut.</p>
+    </div>
+  </article>
+</section>
 
 <?php include "partials/footer.php"; ?>
